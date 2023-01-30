abecc83dfe678307bac008fbbd803211c8a0b7ca
V.D <87772275+vanshid@users.noreply.github.com>
2023-01-30 17:53:07 +0530

    Revert "Django Admin Interface"

--- django_essence/barcelona/players/admin.py
+++ django_essence/barcelona/players/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Players
 
 # Register your models here.
-
-class PlayersAdmin(admin.ModelAdmin):
-    list_display = ('firstName','lastName','dob',)
-
-admin.site.register(Players, PlayersAdmin)
--- django_essence/barcelona/players/models.py
+++ django_essence/barcelona/players/models.py
@@ -6,6 +6,3 @@ class Players(models.Model):
     lastName = models.CharField(max_length=255)
     age = models.IntegerField(null=True)
     dob = models.DateField(null=True)
-
-    def __str__(self):
-        return f"{self.firstName} {self.lastName}"
